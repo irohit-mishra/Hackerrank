86189673044ab0a67802c4aaa0b83cd66f440ec5
Rohit Mishra <97536628+irohit-mishra@users.noreply.github.com>
2024-01-27 23:39:38 +0530

    Add files via upload

---
+++ LoopOne.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class LoopOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+for(int i= 1; i<=10; i++){
+    if (N>=2 && N<=20){
+        System.out.println(N + " x " + i + " = " + N*i);
+    }
+}
+        bufferedReader.close();
+    }
+}
